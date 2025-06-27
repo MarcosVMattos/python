597288526d64e9618550db14768df51cd071f173
Marcos Mattos <marcos98mattos@gmail.com>
2025-06-27 09:37:49 -0300

    estudando POO

---
+++ modulo_2/projeto/jogo.py
@@ -0,0 +1,49 @@
+# Personagem: Classe mãe
+# Herói: Controlado pelo usuário
+# Inimigo: Adversário do usuário
+
+class Personagem:
+    def __init__(self, nome, vida, nivel) -> None:
+        self.__nome = nome
+        self.__vida = vida
+        self.__nivel = nivel
+
+    def get_nome(self):
+        return self.__nome
+    
+    def get_vida(self):
+        return self.__vida
+    
+    def get_nivel(self):
+        return self.__nivel
+    
+    def exibir_detalhes(self):
+        return f"Nome: {self.get_nome()}\nVida: {self.get_vida()}\nNível: {self.get_nivel()}"
+    
+class Heroi(Personagem):
+    def __init__(self, nome, vida, nivel, habilidade) -> None:
+        super().__init__(nome, vida, nivel)
+        self.__habilidade = habilidade
+
+
+    def get_habilidade(self):
+        return self.__habilidade
+    
+    def exibir_detalhes(self):
+        return f"{super().exibir_detalhes()}\nHabilidade: {self.get_habilidade()}\n"
+    
+class Inimigo(Personagem):
+    def __init__(self, nome, vida, nivel, tipo) -> None:
+        super().__init__(nome, vida, nivel)
+        self.__tipo = tipo
+
+    def get_tipo(self):
+        return self.__tipo
+    
+    def exibir_detalhes(self):
+        return f"{super().exibir_detalhes()}\nTipo: {self.get_tipo()}"
+
+heroi = Heroi("Heroi", 100, 7, "Super Força")
+print(heroi.exibir_detalhes())
+inimigo = Inimigo("Inimigo", 100, 6, "Infernal")
+print(inimigo.exibir_detalhes())
