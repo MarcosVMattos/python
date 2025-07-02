045449064494e4bb8ac91ee0a4d1c802e56171af
Marcos Mattos <marcos98mattos@gmail.com>
2025-07-02 20:01:45 -0300

    Update jogo.py

--- modulo_2/projeto/jogo.py
+++ modulo_2/projeto/jogo.py
@@ -20,6 +20,16 @@ class Personagem:
     def exibir_detalhes(self):
         return f"Nome: {self.get_nome()}\nVida: {self.get_vida()}\nNível: {self.get_nivel()}"
     
+    def receber_ataque(self, dano):
+        self.__vida -= dano
+        if self.__vida > 0:
+            self.__vida = 0
+    
+    def atacar(self, alvo):
+        dano = self.__nivel * 2
+        alvo.receber_ataque(dano)
+        print(f"{self.get_nome()} atacou {alvo.get_nome()} e causou {dano} de dano!")
+    
 class Heroi(Personagem):
     def __init__(self, nome, vida, nivel, habilidade) -> None:
         super().__init__(nome, vida, nivel)
@@ -43,7 +53,28 @@ class Inimigo(Personagem):
     def exibir_detalhes(self):
         return f"{super().exibir_detalhes()}\nTipo: {self.get_tipo()}"
 
-heroi = Heroi("Heroi", 100, 7, "Super Força")
-print(heroi.exibir_detalhes())
-inimigo = Inimigo("Inimigo", 100, 6, "Infernal")
-print(inimigo.exibir_detalhes())
+class Jogo:
+    """ Classe orquestradora do jogo """
+
+    def __init__(self) -> None:
+        self.heroi = Heroi("Heroi", 100, 7, "Super Força")
+        self.inimigo = Inimigo("Inimigo", 100, 6, "Infernal")
+
+    def iniciar_batalha(self):
+        """ Fazer a gestão da batalha em turnos """
+        print("Iniciando batalha!")
+        while self.heroi.get_vida() > 0 and self.inimigo.get_vida() > 0:
+            print("\nDetalhes dos Personagens:")
+            print(self.heroi.exibir_detalhes())
+            print(self.inimigo.exibir_detalhes())
+
+            input("Pressione Enter para atacar...")
+            escolha = int(input("Escolha (1 - Ataque normal, 2 - Ataque especial): "))
+
+            if escolha == 1:
+                self.heroi.atacar(self.inimigo)
+            else:
+                print("Escolha inválida. Escolha novamente")
+
+jogo = Jogo()
+jogo.iniciar_batalha()
